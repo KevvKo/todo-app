d504e325f17ff440b6c4cb57c30c3be3ca6a4e23
Kevin Klein <k_klein93@web.de>
2020-07-20 21:56:22 +0200

    change paths and titles

---
+++ Migrations/20200720165121_InitialCreate.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace toDoApp.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ToDoItem",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    name = table.Column<string>(nullable: true),
+                    isDone = table.Column<bool>(nullable: false),
+                    creationDate = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ToDoItem", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ToDoItem");
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -15,20 +15,31 @@ namespace toDoApp
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
+            Environment = env;
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
-
+        public IWebHostEnvironment Environment { get; }
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+            if (Environment.IsDevelopment())
+            {
+                services.AddDbContext<ToDoContext>(options =>
+                options.UseSqlite(
+                    Configuration.GetConnectionString("ToDoContext")));
+            }
+            else
+            {
+                services.AddDbContext<ToDoContext>(options =>
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("ToDoContext")));
+            }
 
-            services.AddDbContext<ToDoContext>(options =>
-            options.UseSqlite(Configuration.GetConnectionString("ToDoContext")));
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
